b5c7d368c6fd7644f6ef8ba52dc52694e95c99f4
DigvijaySinghParmar <digvijayparmar01@gmail.com>
2022-01-06 13:34:35 +0530

    My first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# ls=['a','2','3',5,6,7]
+# ls.append('Hello')
+# print(ls)
+# print(type(ls))
+
+# for i in ls:
+#     print(i)
+
+# ls.pop(0)
+# ls.insert(0,'Digi')
+# ls.insert(2,'Fooker')
+# print(ls[0:])
+# s="Welcome"
+# s1=s[3:0:-1]
+# print(s1)
+
+# a[::-1]    # all items in the array, reversed
+# a[1::-1]   # the first two items, reversed
+# a[:-3:-1]  # the last two items, reversed
+# a[-3::-1]  # everything except the last two items, reversed
+
+# i=5
+# while i<10:
+#     print("Hello World!")
+#     print(i)
+#     i+=1
+
+for i in range(5):
+    print(i)
+    print(type(i))
